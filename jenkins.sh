adf5871129af7b9be78b521a2e97cff94d61133e
lukeshgadge <lukeshgadge89@gmailcom>
2024-12-19 14:33:47 +0530

    update_jenkins_file_1

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,8 @@
 
 #### java install ########
-sudo apt update
-sudo apt install fontconfig openjdk-17-jre
+sudo apt-get update -y
+#sudo apt-get upgrade -y
+sudo apt install fontconfig openjdk-17-jre -y
 java -version
 
 ######## jenkins install #########
@@ -10,8 +11,8 @@ sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get update -y
+sudo apt-get install jenkins -y
 
 ### jenkins check ########
 
